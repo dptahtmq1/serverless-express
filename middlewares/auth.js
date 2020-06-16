5d5b61fc3e620b772455824b0c92b40ed8f412bb
dptahtmq1 <dptahtmq1@gmail.com>
2020-06-16 22:53:18 +0900

    add auth middleware and unit test with `sinon`

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,24 @@
-module.exports = (req, res, next) => {
-  console.log('auth function');
+async function authenticate(req, res, next) {
+  const token = req.headers['authorization'] || '';
+
+  try {
+    await verify(token);
+  } catch (e) {
+    console.error('authentication error: ', e.message);
+    return res.status(401).send(e.message);
+  }
+
   next();
+}
+
+function verify(token) {
+  if (token === 'valid') {
+    return true;
+  } else {
+    throw new Error('invalid token');
+  }
+}
+
+module.exports = {
+  authenticate
 };
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,12 +1,40 @@
-const app = require('../app');
 const expect = require('chai').expect;
 const request = require('supertest');
+const sinon = require('sinon');
+
+const auth = require('../middlewares/auth');
 
 describe('/users', () => {
-    it('GET / ', async () => {
-        const res = await request(app).get('/users');
+  it('GET / ', async () => {
+    // Given
+    sinon.stub(auth, 'authenticate').callsFake((req, res, next) => next());
+    const app = requireUncached('../app');
 
-        expect(res.status).to.equal(200);
-        expect(res.text).to.equal('respond with a resource');
-    });
+    // When
+    const res = await request(app).get('/users');
+
+    // Then
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal('respond with a resource');
+  });
+
+  it('GET / throws 401 when auth token is invalid', async () => {
+    // Given
+    const app = requireUncached('../app');
+
+    // When
+    const res = await request(app).get('/users');
+
+    // Then
+    expect(res.status).to.equal(401);
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
 });
+
+function requireUncached(module) {
+  delete require.cache[require.resolve(module)];
+  return require(module);
+}
